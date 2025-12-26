68b265a417d8dfbb8e7ec7f2d19a77a1389a62a0
Derek Lee Nen That <dereklee0312@Dereks-MacBook-Pro-2.local>
2025-12-26 15:15:30 +0800

    updated mp4 to wav script

--- speech/src/mp4_to_wav.sh
+++ speech/src/mp4_to_wav.sh
@@ -1,4 +1,9 @@
-for file in ../dataset/Validation/video/*.mp4; do
+for file in ../Validation/Videos/*.mp4; do
     filename=$(basename "$file" .mp4)
-    ffmpeg -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../dataset/Validation/audio/${filename}.mp4.wav"
+    ffmpeg -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../Validation/audio/${filename}.mp4.wav"
+done
+
+for file in ../Training/Videos/*.mp4; do
+    filename=$(basename "$file" .mp4)
+    ffmpeg -i "$file" -vn -ac 1 -ar 16000 -acodec pcm_s16le "../Training/audio/${filename}.mp4.wav"
 done
